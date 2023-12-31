8148b2709f193a878fe24b2bff0694971c7864cd
bebomusa <lj6976294@mail.com>
2023-12-31 12:45:26 -0500

    relicense under MIT

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,20 +4,21 @@ game "gta5"
 name "bebo_jail"
 author "BerkieB & Bebo"
 description "Server-side admin jail manager"
-version "1.0.3"
+version "1.0.4"
 repository "https://github.com/bebomusa/bebo_jail"
-license "GPL v3"
+license "MIT"
 
 server_scripts {
 	"@oxmysql/lib/MySQL.lua",
 	"config.lua",
-	"server.lua",
+	"server.lua"
 }
 
 dependencies {
 	"/onesync",
 	"/server:6129",
 	"oxmysql",
+	"ox_inventory"
 }
 
 lua54 "yes"
