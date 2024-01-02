9ce21fe95eb63d6b3f62a28a8c94874e20c4e0cd
bebomusa <lj6976294@mail.com>
2024-01-02 16:33:17 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,12 +4,14 @@ game "gta5"
 name "bebo_jail"
 author "BerkieB & Bebo"
 description "Server-side admin jail manager"
-version "1.0.4"
+version "1.0.5"
 repository "https://github.com/bebomusa/bebo_jail"
 license "MIT"
 
 server_scripts {
 	"@oxmysql/lib/MySQL.lua",
+	"@ox_core/imports/server.lua",
+	"@ox_lib/init.lua",
 	"config.lua",
 	"server.lua"
 }
